628aaf5abd46ce89d371bdb5dc919d9f6be2698c
Seán Roche <35078186+senroche@users.noreply.github.com>
2021-03-17 14:34:22 +0000

    Updating area and patient apis

--- server/routes/areaAPI.js
+++ server/routes/areaAPI.js
@@ -1,19 +1,11 @@
-const AreaModel = require("../db-models/AreaModel.js");
+const Area = require("../db-models/AreaModel.js");
 var randtoken = require('rand-token');
-/*
-{
-	"area":"locationName",
-	"members": ["mem1","mem2","mem3"]
-}
-*/
-exports.createArea = async function (req, res) {
-    var params = req.body
-    try {
-        const area = new AreaModel({
-            name: params.name,
-            members: [],
-        });
 
+
+exports.createArea = async function (req, res) {
+    try {
+        const area = new Area()
+        area.name = req.body.name
         const newArea = await area.save();
         res.status(200).json(newArea);
     }
@@ -31,63 +23,21 @@ exports.createArea = async function (req, res) {
 }
 */
 
-exports.addMember = async function (req, res) {
-    let params = req.body;
-    let tarea = await AreaModel.findOne({ 'name': params.area });
-
-    try {
-
-        tarea.members.push(params.username);
-        const updatedArea = await tarea.save();
-        res.status(200).json(updatedArea);
-    }
-    catch (err) {
-        res.status(400).send({ "error": true, "message": err });
-    }
-};
-
-
-exports.areaByUser = async (req, res) => {
-    await AreaModel.find({ members: req.params.name }, (err, Area) => {
-        if (err) {
-            return res.status(400).json({ success: false, error: "err" })
-        }
-        if (!Area) {
-            return res.status(404).json({ success: false, error: `Area not found` })
-        }
-        var areas = [];
-        for (i = 0; i < Area.length; i++) {
-            console.log(Area[i].name)
-            areas.push(Area[i].name)
-        }
-        res.status(200).json(areas);
-    }).catch(err => console.log(err));
-}
-
-
 exports.getAreas = async (req, res) => {
-    await AreaModel.find({}, (err, Areas) => {
+    query = {}
+    if(req.body.name)
+        query = {name: req.body.name}
+
+    await Area.find(query, (err, Areas) => {
         if (err) {
             return res.status(400).json({ success: false, error: err })
         }
         if (!Areas.length) {
             return res
                 .status(404)
-                .json({ success: false, error: `Area not found` })
+                .json({ success: false, error: `No areas found` })
         }
-        return res.status(200).json({ success: true, data: Areas })
-    }).catch(err => console.log(err))
-}
-
-exports.getArea = async (req, res) => {
-    await AreaModel.find({ name: req.params.name }, (err, Areas) => {
-        if (err) {
-            res(0)
-        }
-        if (!Areas.length) {
-            res(0)
-        }
-        res.send(Areas);
+        return res.status(200).json({ success: true, data: Areas.members })
     }).catch(err => console.log(err))
 }
 
--- server/routes/patientAPI.js
+++ server/routes/patientAPI.js
@@ -1,35 +1,15 @@
-const PatientModel = require("../db-models/PatientModel.js");
-const AreaModel = require("../db-models/AreaModel.js");
+const Patient = require("../db-models/PatientModel.js");
+const Area = require("../db-models/AreaModel.js");
 var CryptoJS = require("crypto-js");
 var config = require('../config');
 const UserModel = require("../db-models/UserModel.js");
 const jwt = require('jsonwebtoken');
 
-async function getArea(area) {
-    var g = await AreaModel.find({ name: area }, (err, Areas) => {
-        if (err) {
-            return (0)
-        }
-        if (!Areas.length) {
-            return (0)
-        }
-        return Areas
-    }).catch(err => console.log(err))
-    return g;
-}
+
 
 // Create new patient 
 exports.newPatient = async function (req, res) {
     let param = req.body;
-    var token = param.token;
-    // Verify token
-    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
-    jwt.verify(token, config.secret, function(err, decoded) {
-        if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-        if(UserModel.findById(decoded)==[]) return res.status(500).send({ auth: false, message: 'Token expired/invalid.' });
-        return
-    });
-
 
     // Encrypt patient data before adding it to the db (Just to say we considered it, obviously not effective)
     var cipher_name = CryptoJS.AES.encrypt(JSON.stringify(param.name), config.secret).toString();
@@ -37,53 +17,43 @@ exports.newPatient = async function (req, res) {
     var cipher_weight = CryptoJS.AES.encrypt(JSON.stringify(param.weight), config.secret).toString();
     var cipher_height = CryptoJS.AES.encrypt(JSON.stringify(param.height), config.secret).toString();
 
-    var post = new PostModel({
-        area_name: param.area,
-        addedBy: param.addedBy,
+    var post = new Patient({
         name: cipher_name,
         age: cipher_age,
         height:cipher_height,
         weight: cipher_weight,
         date: Date.now()
     });
+
     const newPatient = await post.save();
-    if (newPatient != post) {
-        res.send(400).json("Error");
+
+    // Add patient to area (if doesn't exist, create)
+    let tarea = await Area.findOne({ 'name': param.area });
+
+    try {
+        if(tarea == null){
+            tarea = new Area()
+            tarea.name = param.area
+        }
+
+        tarea.members.push(newPatient);
+        const updatedArea = await tarea.save();
+        res.status(200).json(updatedArea);
     }
-    res.send(200).json(newPatient);
+    catch (err) {
+        res.status(400).send(tarea);
 };
+}
 
-
-
-//Get area name -> use key to decrypt
+// Get all patients
 exports.getPatients = async function (req, res) {
     let param = req.body;
-    var token = param.token;
-
-    // Verify token
-    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
-    jwt.verify(token, config.secret, function(err, decoded) {
-        if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-        if(UserModel.findById(decoded)==[]) return res.status(500).send({ auth: false, message: 'Token expired/invalid.' });
-        return
-    });
-
-
-    // Get all patient data
-    if(param.area){
-        const getPatients = await PatientModel.find(req.body.area);
-        var patients = getPatients;
-    }
-    else{
-        const getPatients = await PatientModel.find({});
-        var patients = getPatients;
-    }
+    const getPatients = await Patient.find({});
+    var patients = getPatients;
 
     for (var i = 0; i < patients.length; i++) {
             var key = config.secret
             var decryptedPatient = {
-                area_name: patients[i].area,
-                addedBy: patients[i].addedBy,
                 name: CryptoJS.AES.decrypt(patients[i].name, key).toString(CryptoJS.enc.Utf8),
                 age: CryptoJS.AES.decrypt(patients[i].age, key).toString(CryptoJS.enc.Utf8),
                 height:CryptoJS.AES.decrypt(patients[i].weight, key).toString(CryptoJS.enc.Utf8),
